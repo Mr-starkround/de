b962812ea0084b6bb177ed4d42178d7b459ae3be
Noob <78058269+Mr-starkround@users.noreply.github.com>
2024-01-01 01:27:13 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -16,15 +16,7 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
     username = f"@{msg.from_user.username}".lower() if msg.from_user.username else None
     if username and username not in msg.text.lower():
         return await msg.reply('Anda hanya dapat mengirim menfess dengan menggunakan username Anda sendiri.', quote=True)
-
-    # Check if the user is authorized to send messages
-    if user.status not in ['owner', 'admin', 'talent', 'daddy sugar']:
-        # Check if the message mentions usernames from the admin list
-        admin_usernames = ["@ownneko", "@satt329", "@nekojoyy", "@winnieewwe", "@mwehehe0j", "@ikeenandrasw", "@sasaanmf", "@lordmudaid", "@towirg", "@suunshiinneee", "@kjitten"]
-        for admin_username in admin_usernames:
-            if admin_username in msg.text.lower():
-                return await msg.reply(f'Maaf, Anda tidak diizinkan mengirim pesan yang mengandung username member premium {admin_username}.', quote=True)
-
+ 
     # Check for URLs in the message
     if re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", msg.text or ""):
         return await msg.reply("Tidak diizinkan mengirimkan tautan.")
@@ -40,6 +32,7 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
                 return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi. \n\n\n\n Info: Topup Coin Hanya ke @OwnNeko', quote=True)
 
         if key == config.hastag.split('|')
+          return await msg.reply(pakai hastag.)
 
         link = await get_link()
         caption = msg.text or msg.caption
