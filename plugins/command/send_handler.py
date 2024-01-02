7334d0e53aa10d46fe2f24439cd38e6673b9ab26
Noob <78058269+Mr-starkround@users.noreply.github.com>
2024-01-02 11:52:07 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -19,9 +19,10 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
             else:
                 return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi. \n\n\n\n Info: Topup Coin Hanya ke @OwnNeko', quote=True)
 
-        if key == hastag[0]:            
+        if key == hastag[0]: 
+         picture = msg.photo_from_user.id           
         elif key == hastag[1]:
-            
+        picture = msg.video_from_user.id               
 
         link = await get_link()
         caption = msg.text or msg.caption
@@ -30,7 +31,7 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
         # Remove URLs from caption
         caption = re.sub(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "", caption)
 
-        kirim = await client.send_photo(config.channel_1, caption, caption_entities=entities)
+        kirim = await client.send_photo(config.channel_1, picture, caption, caption_entities=entities)
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
         await db.update_menfess(coin, menfess, all_menfess)
         await msg.reply(f"pesan telah berhasil terkirim. hari ini kamu telah mengirim menfess sebanyak {menfess + 1}/{config.batas_kirim} . kamu dapat mengirim menfess sebanyak {config.batas_kirim} kali dalam sehari\n\nwaktu reset setiap jam 1 pagi\n<a href='{link + str(kirim.id)}'>check pesan kamu</a>. \n\n\n\n Info: Topup Coin Hanya ke @OwnNeko")
