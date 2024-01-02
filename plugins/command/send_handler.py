dd72829cb0276fad7068a13d42dd6f3d6372fe37
Noob <78058269+Mr-starkround@users.noreply.github.com>
2024-01-02 11:29:30 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -3,45 +3,10 @@ import re
 from pyrogram import Client, types, enums
 from plugins import Database, Helper
 
-async def send_menfess(client: Client, msg: types.Message, hastag: list):
-    helper = Helper(client, msg)
+async def send_with_pic_handler(client: Client, msg: types.Message, key: str, hastag: list):
     db = Database(msg.from_user.id)
-    db_user = db.get_data_pelanggan()
-    db_bot = db.get_data_bot(client.id_bot).kirimchannel
- 
-    if msg.text or msg.photo or msg.video or msg.voice:
-        if msg.photo and not db_bot.photo:
-            if db_user.status in ['member', 'talent']:
-                return await msg.reply('Tidak bisa mengirim photo, karena sedang dinonaktifkan oleh admin', True)
-        elif msg.video and not db_bot.video:
-            if db_user.status in ['member']:
-                return await msg.reply('Tidak bisa mengirim video, karena sedang dinonaktifkan oleh admin', True)
-        elif msg.voice and not db_bot.voice:
-            return await msg.reply('Tidak bisa mengirim voice, karena sedang dinonaktifkan oleh admin', True)
-
-        menfess = db_user.menfess
-        all_menfess = db_user.all_menfess
-        coin = db_user.coin
-        if menfess >= config.batas_kirim and db_user.status in ['member', 'talent']:
-            if coin >= config.biaya_kirim:
-                coin = db_user.coin - config.biaya_kirim
-            else:
-                return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi', quote=True)
-
-
-        link = await get_link()
-        kirim = await client.copy_message(config.channel_1, msg.from_user.id, msg.id)
-        await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
-        await db.update_menfess(coin, menfess, all_menfess)
-        await msg.reply(f"pesan telah berhasil terkirim. hari ini kamu telah mengirim menfess sebanyak {menfess + 1}/{config.batas_kirim} . kamu dapat mengirim menfess sebanyak {config.batas_kirim} kali dalam sehari\n\nwaktu reset setiap jam 1 pagi\n<a href='{link + str(kirim.id)}'>check pesan kamu</a>")
-    else:
-        await msg.reply('media yang didukung photo, video dan voice')
-
-async def send_menfess_handler(client: Client, msg: types.Message, key: str, hastag: list):
     helper = Helper(client, msg)
-    db = Database(msg.from_user.id)
-    db_user = db.get_data_pelanggan()
-    db_bot = db.get_data_bot(client.id_bot).kirimchannel
+    user = db.get_data_pelanggan()
 
     # Check if the sender has a username
     if msg.from_user.username is None:
@@ -49,13 +14,71 @@ async def send_menfess_handler(client: Client, msg: types.Message, key: str, has
 
     # Check if the message mentions the sender's username
     username = f"@{msg.from_user.username}".lower() if msg.from_user.username else None
-    if username and username not in msg.text:
+    if username and username not in msg.text.lower():
         return await msg.reply('Anda hanya dapat mengirim menfess dengan menggunakan username Anda sendiri.', quote=True)
+
+    # Check if the user is authorized to send messages
+    if user.status not in ['owner', 'admin', 'talent', 'daddy sugar']:
+        # Check if the message mentions usernames from the admin list
+        admin_usernames = ["@ownneko", "@satt329", "@nekojoyy", "@winnieewwe", "@mwehehe0j", "@ikeenandrasw", "@sasaanmf", "@lordmudaid", "@towirg", "@suunshiinneee", "@kjitten"]
+        for admin_username in admin_usernames:
+            if admin_username in msg.text.lower():
+                return await msg.reply(f'Maaf, Anda tidak diizinkan mengirim pesan yang mengandung username member premium {admin_username}.', quote=True)
 
     # Check for URLs in the message
     if re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", msg.text or ""):
         return await msg.reply("Tidak diizinkan mengirimkan tautan.")
 
+    if msg.text or msg.photo or msg.video or msg.voice:
+        menfess = user.menfess
+        all_menfess = user.all_menfess
+        coin = user.coin
+        if menfess >= config.batas_kirim and user.status in ['member', 'talent']:
+            if coin >= config.biaya_kirim:
+                coin = user.coin - config.biaya_kirim
+            else:
+                return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi. \n\n\n\n Info: Topup Coin Hanya ke @OwnNeko', quote=True)
+
+        if key == hastag[0]:
+            picture = config.pic_girl
+        elif key == hastag[1]:
+            picture = config.pic_boy
+
+        if user.status == 'talent':
+            picture = config.pic_talentgirl
+        if user.status == 'owner':
+            picture = config.pic_owner
+        if user.status == 'admin':
+            if key == hastag[0]:
+                picture = config.pic_admingirl
+            elif key == hastag[1]:
+                picture = config.pic_adminboy
+        if user.status == 'daddy sugar':
+            picture = config.pic_daddysugar
+        if user.status == 'boyfriend rent':
+            picture = config.pic_bfrent
+        elif user.status == 'moans boy':
+            picture = config.pic_moansboy
+
+        link = await get_link()
+        caption = msg.text or msg.caption
+        entities = msg.entities or msg.caption_entities
+
+        # Remove URLs from caption
+        caption = re.sub(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "", caption)
+
+        kirim = await client.send_photo(config.channel_1, picture, caption, caption_entities=entities)
+        await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
+        await db.update_menfess(coin, menfess, all_menfess)
+        await msg.reply(f"pesan telah berhasil terkirim. hari ini kamu telah mengirim menfess sebanyak {menfess + 1}/{config.batas_kirim} . kamu dapat mengirim menfess sebanyak {config.batas_kirim} kali dalam sehari\n\nwaktu reset setiap jam 1 pagi\n<a href='{link + str(kirim.id)}'>check pesan kamu</a>. \n\n\n\n Info: Topup Coin Hanya ke @OwnNeko")
+    else:
+        await msg.reply('media yang didukung photo, video dan voice')
+
+async def send_menfess_handler(client: Client, msg: types.Message):
+    helper = Helper(client, msg)
+    db = Database(msg.from_user.id)
+    db_user = db.get_data_pelanggan()
+    db_bot = db.get_data_bot(client.id_bot).kirimchannel
     if msg.text or msg.photo or msg.video or msg.voice:
         if msg.photo and not db_bot.photo:
             if db_user.status in ['member', 'talent']:
@@ -75,8 +98,24 @@ async def send_menfess_handler(client: Client, msg: types.Message, key: str, has
             else:
                 return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi', quote=True)
 
- 
         link = await get_link()
+
+        # Check if the message mentions the sender's username
+        username = f"@{msg.from_user.username}".lower() if msg.from_user.username else None
+
+        # Check if the message contains mentions of other usernames
+        if msg.entities:
+            for entity in msg.entities:
+                if entity.type == "mention":
+                    mentioned_username = msg.text[entity.offset:entity.offset + entity.length].lower()
+                    # If the mentioned username is not the sender's username, reject the message
+                    if mentioned_username != username:
+                        return await msg.reply('Anda hanya dapat mengirim menfess dengan menggunakan username Anda sendiri.', quote=True)
+
+        # Use regular expression to check for links in the message
+        if re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", msg.text or ""):
+            return await msg.reply("Tidak diizinkan mengirimkan tautan.", quote=True)
+
         kirim = await client.copy_message(config.channel_1, msg.from_user.id, msg.id)
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
         await db.update_menfess(coin, menfess, all_menfess)
