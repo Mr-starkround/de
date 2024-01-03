c4a506c7a822fffd01ba59b542427749d4ffc7de
Noob <78058269+Mr-starkround@users.noreply.github.com>
2024-01-04 00:46:08 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -79,23 +79,20 @@ async def send_menfess_handler(client: Client, msg: types.Message, link: str = N
 
         link = await get_link()
 
+    # Check if the sender has a username
+    if msg.from_user.username is None:
+        return await msg.reply('Anda harus memiliki username untuk mengirim menfess.', quote=True)
 
-        # Check if the message mentions the sender's username
-        username = f"@{msg.from_user.username}".lower() if msg.from_user.username else None
+    # Check if the message mentions the sender's username
+    username = f"@{msg.from_user.username}".lower() if msg.from_user.username else None
+    if not username and username in msg.text.lower():
+        return await msg.reply('Anda hanya dapat mengirim menfess dengan menggunakan username Anda sendiri.', quote=True)
 
-        # Check if the message contains mentions of other usernames
-        if msg.entities:
-            for entity in msg.entities:
-                if entity.type == "mention":
-                    mentioned_username = msg.text[entity.offset:entity.offset + entity.length].lower()
-                    # If the mentioned username is not the sender's username, reject the message
-                    if mentioned_username != username:
-                        return await msg.reply('Anda hanya dapat mengirim menfess dengan menggunakan username Anda sendiri.', quote=True)
+    # Check for URLs in the message
+    if re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", msg.text or ""):
+        return await msg.reply("Tidak diizinkan mengirimkan tautan.")
 
-        # Use regular expression to check for links in the message
-        if re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", msg.text or ""):
-            return await msg.reply("Tidak diizinkan mengirimkan tautan.", quote=True)        
-
+ 
 
         kirim = await client.copy_message(config.channel_1, msg.from_user.id, msg.id)
 
